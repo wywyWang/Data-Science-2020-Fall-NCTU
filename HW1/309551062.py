8fc9ded37c73cab9d65bd6c2d826df43898d1249
yao <sf163864@gmail.com>
2020-09-24 13:25:56 +0800

    first upload

---
+++ HW1/309551062.py
@@ -0,0 +1,14 @@
+import sys
+
+
+if __name__ == '__main__':
+    #get parameters
+    print(sys.argv)
+    
+    # function crawl
+    
+    # function push
+    
+    # function popular
+    
+    # function keyword
