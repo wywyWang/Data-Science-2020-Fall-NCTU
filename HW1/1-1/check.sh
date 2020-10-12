33fdfb6bb711af533b0e9c0c9e992ea4f0f29631
yao <sf163864@gmail.com>
2020-10-12 19:37:27 +0800

    add some lazy script to test

---
+++ HW1/1-1/check.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+bash -c 'diff <(sort "./keyword(新莊)[101-1231].txt") <(sort "./refer/Q4/keyword(新莊)[101-1231].txt") > "./refer/Compare/keyword(新莊)[101-1231].txt"'
+bash -c 'diff <(sort "./keyword(baby)[101-1231].txt") <(sort "./refer/Q4/keyword(baby)[101-1231].txt") > "./refer/Compare/keyword(baby)[101-1231].txt"'
+bash -c 'diff <(sort "./keyword(QQ)[101-1231].txt") <(sort "./refer/Q4/keyword(QQ)[101-1231].txt") > "./refer/Compare/keyword(QQ)[101-1231].txt"'
+bash -c 'diff <(sort "./keyword(健身)[101-1231].txt") <(sort "./refer/Q4/keyword(健身)[101-1231].txt") > "./refer/Compare/keyword(健身)[101-1231].txt"'
+bash -c 'diff <(sort "./keyword(神人)[101-1231].txt") <(sort "./refer/Q4/keyword(神人)[101-1231].txt") > "./refer/Compare/keyword(神人)[101-1231].txt"'
+bash -c 'diff <(sort "./keyword(西門)[101-1231].txt") <(sort "./refer/Q4/keyword(西門)[101-1231].txt") > "./refer/Compare/keyword(西門)[101-1231].txt"'
+bash -c 'diff <(sort "./keyword(陽光)[101-1231].txt") <(sort "./refer/Q4/keyword(陽光)[101-1231].txt") > "./refer/Compare/keyword(陽光)[101-1231].txt"'
+bash -c 'diff <(sort "./keyword(黑嘉嘉)[101-1231].txt") <(sort "./refer/Q4/keyword(黑嘉嘉)[101-1231].txt") > "./refer/Compare/keyword(黑嘉嘉)[101-1231].txt"'
+bash -c 'diff <(sort "./keyword(正妹)[101-1231].txt") <(sort "./refer/Q4/keyword(正妹)[101-1231].txt") > "./refer/Compare/keyword(正妹)[101-1231].txt"'
---
+++ HW1/1-1/lazy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python 309551062.py keyword baby 101 1231
+python 309551062.py keyword QQ 101 1231
+python 309551062.py keyword 健身 101 1231
+python 309551062.py keyword 黑嘉嘉 101 1231
+python 309551062.py keyword 神人 101 1231
+python 309551062.py keyword 陽光 101 1231
+python 309551062.py keyword 新莊 101 1231
+python 309551062.py keyword 西門 101 1231
+python 309551062.py keyword 正妹 101 1231
