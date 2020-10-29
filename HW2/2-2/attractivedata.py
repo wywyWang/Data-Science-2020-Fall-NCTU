e0fc9e1bde623b6179100e6a8b0471e996e9feac
yao <sf163864@gmail.com>
2020-10-29 11:11:14 +0800

    add category as embedding to concate with CLS vector, but testing has bug to fix

--- HW2/2-2/attractivedata.py
+++ HW2/2-2/attractivedata.py
@@ -19,21 +19,33 @@ class AttractiveData:
         self.df_train = pd.read_csv(train_file)
         self.df_test = pd.read_csv(test_file)
 
-        self.TEXT = data.Field(sequential=True, init_token='<s>', lower=True, tokenize=self.tokenizer)
+        self.df_test[['ID', 'Headline', 'Category']].to_csv('./data/new_test.csv', index=False)
+
+        self.TEXT = data.Field(sequential=True, init_token='<s>', lower=False, tokenize=self.tokenizer, fix_length=max_size, pad_token='0')
         self.LABEL = data.Field(dtype=torch.float, sequential=False, use_vocab=False)
         self.CATEGORIES_LABEL = data.LabelField(sequential=False)
+        # self.CATEGORIES_LABEL = data.Field(sequential=False, use_vocab=False)
 
-        self.train_data, self.test_data = data.TabularDataset.splits(
-            path=path, train=train_filename, test=test_filename, format="csv", skip_header=True, 
+        # self.train_data, self.test_data = data.TabularDataset.splits(
+        #     path=path, train=train_filename, test=test_filename, format="csv", skip_header=True, 
+        #     fields=[('ID', None), ('Headline', self.TEXT), ('Category', self.CATEGORIES_LABEL), ('Label', self.LABEL)]
+        # )
+
+        self.train_data = data.TabularDataset(
+            path=train_file, format="csv", skip_header=True, 
             fields=[('ID', None), ('Headline', self.TEXT), ('Category', self.CATEGORIES_LABEL), ('Label', self.LABEL)]
+        )
+        self.test_data = data.TabularDataset(
+            path='./data/new_test.csv', format="csv", skip_header=True, 
+            fields=[('ID', None), ('Headline', self.TEXT), ('Category', self.CATEGORIES_LABEL)]
         )
 
         self.TEXT.build_vocab(self.train_data, vectors=pretrained_file, min_freq=min_freq)
-        # self.LABEL.build_vocab(self.train_data)
+        self.LABEL.build_vocab(self.train_data)
         self.CATEGORIES_LABEL.build_vocab(self.train_data)
 
-        self.trainloader = data.BucketIterator(self.train_data, sort_key=lambda x: len(x.Text), batch_size=batch_size, device=self.device)
-        self.testloader = data.BucketIterator(self.test_data, sort_key=lambda x: len(x.Text), batch_size=batch_size, device=self.device)
+        self.trainloader = data.BucketIterator(self.train_data, sort_key=lambda x: len(x.Text), batch_size=batch_size, device=self.device, train=True)
+        self.testloader = data.BucketIterator(self.test_data, sort=False, batch_size=batch_size, device=self.device, train=False)
 
     def tokenizer(self, corpus):
         return [str(token) for token in self.nlp_model(corpus)]
--- HW2/2-2/trainer.py
+++ HW2/2-2/trainer.py
@@ -2,25 +2,25 @@ import torch.nn as nn
 import torch
 import tqdm
 import math
-# from attractivenet import AttractiveNet
 from transformermodel import TransformerModel
 
 class AttractiveTrainer:
 
-    def __init__(self, save_name, log_steps, epochs, lr, timestr, device, train_loader, test_loader, input_dim, embedding_dim, hidden_dim, output_dim, pretrained_embeddings, dropout, num_layers, nhead):
+    def __init__(self, save_name, log_steps, epochs, lr, timestr, device, train_loader, test_loader, input_dim, category_dim, category_embedding_dim, embedding_dim, hidden_dim, output_dim, pretrained_embeddings, dropout, num_layers, nhead):
         self.criterion = torch.nn.MSELoss()
         self.save_name = save_name
         self.log_steps = log_steps
         self.epochs = epochs
         self.lr = lr
         self.device = device
-        # self.model = AttractiveNet(input_dim, embedding_dim, hidden_dim, output_dim, dropout, num_layers).to(self.device)
-        self.model = TransformerModel(nhead, input_dim, embedding_dim, hidden_dim, output_dim, dropout, num_layers).to(self.device)
+        self.model = TransformerModel(nhead, input_dim, category_dim, category_embedding_dim, embedding_dim, hidden_dim, output_dim, dropout, num_layers).to(self.device)
         self.model.embedding.token.weight = nn.Parameter(pretrained_embeddings.to(self.device), requires_grad=False)
-        # self.model.embedding.token.weight.requires_grad = False                 # freeze embedding
 
         # self.optimizer = torch.optim.Adam(self.model.parameters(), lr=lr, betas=(0.9, 0.999), weight_decay=0.01)
-        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=lr)
+        # self.optimizer = torch.optim.SGD(self.model.parameters(), lr=lr)
+        self.optimizer = torch.optim.SGD([{'params': self.model.transformer_encoder.parameters(), 'lr': 1e-5}, 
+                                            {'params': self.model.embedding.parameters(), 'lr': 1e-5},
+                                            {'params': self.model.linear.parameters(), 'lr': 1e-4}])
         # self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, 1.0, gamma=0.95)
         # self.scheduler = torch.optim.ScheduledOptim(self.optimizer, self.model.hidden, n_warmup_steps=50)
         self.timestr = timestr
@@ -29,31 +29,35 @@ class AttractiveTrainer:
 
     def train(self):
         for epoch in range(self.epochs):
+            print("Epoch {}".format(epoch))
             self.iteration(epoch)
             # self.scheduler.step()
-            # self.save(epoch)
+        self.save(self.timestr, self.epochs, self.train_loss)
 
     def iteration(self, epoch):
         self.model.train()
-        data_iter = tqdm.tqdm(enumerate(self.train_loader),
-                            desc="EP:{} | lr: {}".format(epoch, self.lr),
-                            total=len(self.train_loader),
-                            bar_format="{l_bar}{r_bar}")
+        # data_iter = tqdm.tqdm(enumerate(self.train_loader),
+        #                     desc="EP:{} | lr: {}".format(epoch, self.lr),
+        #                     total=len(self.train_loader),
+        #                     bar_format="{l_bar}{r_bar}")
         
         avg_loss = 0.0
-        for i, data in data_iter:
+        for i, data in enumerate(self.train_loader):
             inputs = data.Headline
             attractive_labels = data.Label
+            attractive_categories = data.Category
 
             # forward masked_lm model
-            attractive_prediction = self.model(inputs)
+            attractive_prediction = self.model(inputs, attractive_categories)
 
-            # print(inputs)
+            # print(inputs, flush=True)
             # print(attractive_labels)
             # print(attractive_prediction)
+            # print(attractive_categories)
             # print(inputs.shape)
             # print(attractive_labels.shape)
             # print(attractive_prediction.shape)
+            # print(attractive_categories.shape)
             # print(self.criterion(attractive_prediction, attractive_labels).item())
             # 1/0
 
@@ -79,7 +83,7 @@ class AttractiveTrainer:
                     f_train.write(str(post_fix) + '\n')
 
         # evaluate training accuracy
-        self.evaluate(self.train_loader, 'train')
+        attractive_predict, attractive_true, self.train_loss = self.evaluate(self.train_loader, 'train')
 
     def evaluate(self, data_loader, str_code):
         self.model.eval()
@@ -97,9 +101,10 @@ class AttractiveTrainer:
             for i, data in data_iter:
                 inputs = data.Headline
                 attractive_labels = data.Label
+                attractive_categories = data.Category
 
                 # forward masked_lm model
-                attractive_prediction = self.model(inputs)
+                attractive_prediction = self.model(inputs, attractive_categories)
 
                 # MSELoss
                 loss = self.criterion(attractive_prediction, attractive_labels)
@@ -114,8 +119,8 @@ class AttractiveTrainer:
         print()
         print("EP_{} | avg_loss: {} |".format(str_code, avg_loss))
 
-        return attractive_predict.cpu().detach().tolist(), attractive_true.cpu().detach().tolist()
+        return attractive_predict.cpu().detach().tolist(), attractive_true.cpu().detach().tolist(), avg_loss
 
-    def save(self, epoch):
-        output_name = self.save_name + '.' + str(epoch)
+    def save(self, timestr, epochs, loss):
+        output_name = self.save_name + '_' + str(timestr) + '_' + str('{:.3f}'.format(loss)) + '.' + str(epochs)
         torch.save(self.model.state_dict(), output_name)
