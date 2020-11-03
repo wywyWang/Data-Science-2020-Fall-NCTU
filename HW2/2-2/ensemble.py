89168244a98ae3e617e781f051c7fad71fc3fb4b
yao <sf163864@gmail.com>
2020-11-03 20:30:39 +0800

    'CNN_LSTM_20201103-150358_0.4101.70.csv', 'CNN_LSTM_20201103-160026_0.4157.65.csv', 'CNN_LSTM_20201103-163042_0.4105.65.csv', 'CNN_LSTM_20201103-174348_0.4127.70.csv', 'CNN_LSTM_20201103-181044_0.4284.50.csv', 'CNN_LSTM_20201103-181740_0.4171.60.csv', 'CNN_LSTM_20201103-182128_0.4007.65.csv', 'CNN_LSTM_20201103-183552_0.4084.65.csv', 'CNN_LSTM_20201103-184418_0.4154.55.csv', test score: 0.45984

--- HW2/2-2/ensemble.py
+++ HW2/2-2/ensemble.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 if __name__ == "__main__":
-    model_list = ['CNN_LSTM_20201103-181740_0.4408.50.csv', 'CNN_LSTM_20201103-181044_0.4284.50.csv', 'CNN_LSTM_20201103-174348_0.4127.70.csv', 'CNN_LSTM_20201103-163042_0.3972.70.csv', 'CNN_LSTM_20201103-160026_0.4157.65.csv', 'CNN_LSTM_20201103-150358_0.4101.70.csv']
+    model_list = ['CNN_LSTM_20201103-150358_0.4101.70.csv', 'CNN_LSTM_20201103-160026_0.4157.65.csv', 'CNN_LSTM_20201103-163042_0.4105.65.csv', 'CNN_LSTM_20201103-174348_0.4127.70.csv', 'CNN_LSTM_20201103-181044_0.4284.50.csv', 'CNN_LSTM_20201103-181740_0.4171.60.csv', 'CNN_LSTM_20201103-182128_0.4007.65.csv', 'CNN_LSTM_20201103-183552_0.4084.65.csv', 'CNN_LSTM_20201103-184418_0.4154.55.csv']
     folder = './predict/'
     predict = [0] * 227         # 227: test size
     for idx, model_name in enumerate(model_list):
