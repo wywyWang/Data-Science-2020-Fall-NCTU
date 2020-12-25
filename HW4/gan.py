419778c178a9b8731cce9274802228eccac20399
yao <sf163864@gmail.com>
2020-12-25 10:52:24 +0800

    DCGAN, FID=289

--- HW4/gan.py
+++ HW4/gan.py
@@ -37,73 +37,58 @@ def weights_init_normal(m):
         torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
         torch.nn.init.constant_(m.bias.data, 0.0)
 
-
-# class Generator(nn.Module):
-#     def __init__(self):
-#         super(Generator, self).__init__()
-
-#         # [TODO] finish the generator design
-
-#     def forward(self, z):
-#         # z, shape=(opt.batch_size, opt.latent_dim)
-#         # [TODO] finish the forward process
-#         # img, shape=(opt.batch_size, opt.channels, opt.img_size, opt.img_size)
-#         return img
-
 class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
 
-        def block(in_feat, out_feat, normalize=True):
-            layers = [nn.Linear(in_feat, out_feat)]
-            if normalize:
-                layers.append(nn.BatchNorm1d(out_feat, 0.8))
-            layers.append(nn.LeakyReLU(0.2, inplace=True))
-            return layers
+        self.init_size = opt.img_size // 4
+        self.l1 = nn.Sequential(nn.Linear(opt.latent_dim, 128 * self.init_size ** 2))
 
-        self.model = nn.Sequential(
-            *block(opt.latent_dim, 128, normalize=False),
-            *block(128, 256),
-            *block(256, 512),
-            *block(512, 1024),
-            nn.Linear(1024, int(np.prod(img_shape))),
-            nn.Tanh()
+        self.conv_blocks = nn.Sequential(
+            nn.BatchNorm2d(128),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 128, 3, stride=1, padding=1),
+            nn.BatchNorm2d(128, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 64, 3, stride=1, padding=1),
+            nn.BatchNorm2d(64, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(64, opt.channels, 3, stride=1, padding=1),
+            nn.Tanh(),
         )
 
     def forward(self, z):
-        img = self.model(z)
-        img = img.view(img.size(0), *img_shape)
+        out = self.l1(z)
+        out = out.view(out.shape[0], 128, self.init_size, self.init_size)
+        img = self.conv_blocks(out)
         return img
-
-
-# class Discriminator(nn.Module):
-#     def __init__(self):
-#         super(Discriminator, self).__init__()
-
-#         # [TODO] finish the discriminator design
-
-#     def forward(self, img):
-#         # img, shape=(opt.batch_size, opt.channels, opt.img_size, opt.img_size)
-#         # [TODO] finish the forward process
-#         # validity, shape=(opt.batch_size, 1)
-#         return validity
 
 class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
 
+        def discriminator_block(in_filters, out_filters, bn=True):
+            block = [nn.Conv2d(in_filters, out_filters, 3, 2, 1), nn.LeakyReLU(0.2, inplace=True), nn.Dropout2d(0.25)]
+            if bn:
+                block.append(nn.BatchNorm2d(out_filters, 0.8))
+            return block
+
         self.model = nn.Sequential(
-            nn.Linear(int(np.prod(img_shape)), 512),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(512, 256),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(256, 1),
-            nn.Sigmoid(),
+            *discriminator_block(opt.channels, 16, bn=False),
+            *discriminator_block(16, 32),
+            *discriminator_block(32, 64),
+            *discriminator_block(64, 128),
         )
 
+        # The height and width of downsampled image
+        ds_size = opt.img_size // 2 ** 4
+        self.adv_layer = nn.Sequential(nn.Linear(128 * ds_size ** 2, 1), nn.Sigmoid())
+
     def forward(self, img):
-        img_flat = img.view(img.size(0), -1)
-        validity = self.model(img_flat)
+        out = self.model(img)
+        out = out.view(out.shape[0], -1)
+        validity = self.adv_layer(out)
 
         return validity
 
@@ -162,7 +147,8 @@ def train(opt):
 
     dataloader = torch.utils.data.DataLoader(DogDataset('data/images_crop'),
                                              batch_size=opt.batch_size,
-                                             shuffle=True)
+                                             shuffle=True,
+                                             num_workers=opt.n_cpu)
 
     # Optimizers
     optimizer_G = torch.optim.Adam(generator.parameters(),
@@ -327,7 +313,7 @@ if __name__ == '__main__':
                         help="number of epochs of training")
     parser.add_argument("--batch_size",
                         type=int,
-                        default=32,
+                        default=64,
                         help="size of the batches")
     parser.add_argument("--lr",
                         type=float,
@@ -361,6 +347,11 @@ if __name__ == '__main__':
                         type=int,
                         default=100,
                         help="number of generated images for inference")
+    parser.add_argument("--n_cpu", 
+                        type=int, 
+                        default=8, 
+                        help="number of cpu threads to use during batch generation")
+
     opt = parser.parse_args()
 
     global img_shape
